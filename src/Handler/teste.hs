1183f1f08280947c3d51059ce79a9eb42d24a03f
Juciara Lima <juciara.slima@gmail.com>
2018-11-29 12:04:58 +0000

    Configuração do banco

--- src/Handler/teste.hs
+++ src/Handler/teste.hs
@@ -1,1 +1,11 @@
-teste de commit
+--Para entrar no banco pelo terminal:
+--psql -h ec2-184-73-197-211.compute-1.amazonaws.com -U klkhbkahscxqvo -d df1i2trds9ljpv 
+
+--senha:
+--516a16fd97354f929c88605e8c605057b2425246dbd3e54d3fd88de1f56ab234
+
+--para ver as tabelas existentes:
+--\dt
+
+--para ver o conteúdo de cada tabela
+--selec * from nomeTabela
